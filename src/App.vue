61c084343fb691d9d8e513296eaf78f1b9584efc
liujinfeng <1690452198@qq.com>
2021-08-23 10:11:58 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,14 @@
 <template>
-  <div id="app">
-   
-  </div>
+  <div id="app"></div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  components: {
-   
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
 
 <style>
-@import './assets/css/base.css'
+@import "./assets/css/base.css";
 </style>
