5cc61092b4e001ebc3bd37f5735baa30781b2eeb
liujinfeng <1690452198@qq.com>
2021-08-24 16:49:51 +0800

    update

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -2,12 +2,121 @@
   <div id="home">
     <NavBar class="home-nav"><div slot="center">购物街</div> </NavBar>
     <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends='recommends'></recommend-view>
+    <feature-view></feature-view>
+    <tab-control :titles="['流行','新款','精选']" class="tab-contr"></tab-control>
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
   </div>
 </template>
 
 <script>
+import HomeSwiper from "./childComps/HomeSwiper";
+import RecommendView from './childComps/RecommendView';
+import FeatureView from './childComps/FeatureView';
+
 import NavBar from "components/common/navbar/NavBar";
-import HomeSwiper from "./childComps/HomeSwiper.vue";
+import TabControl from 'components/content/tabControl/TabControl'
 
 import { getHomeMultidata } from "network/home";
 
@@ -16,6 +125,9 @@ export default {
   components: {
     NavBar,
     HomeSwiper,
+    RecommendView,
+    FeatureView,
+    TabControl
   },
   data() {
     return {
@@ -34,7 +146,21 @@ export default {
 </script>
 
 <style scoped>
+#home{
+  padding-top: 44px;
+}
 .home-nav {
   background-color: var(--color-tint);
+  color: #fff;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 999;
+}
+.tab-contr{
+  position: sticky;
+  top:44px;
+  background-color: #fff;
 }
 </style>
