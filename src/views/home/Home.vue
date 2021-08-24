92f5b3fdf1967a61ebce3364924705954365a30f
liujinfeng <1690452198@qq.com>
2021-08-24 14:09:50 +0800

    update

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,35 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <NavBar class="home-nav"><div slot="center">购物街</div> </NavBar>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: "Home"
-  }
+import NavBar from "components/common/navbar/NavBar";
+import { getHomeMultidata } from "network/home";
+export default {
+  name: "Home",
+  components: {
+    NavBar,
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+    };
+  },
+  created() {
+    getHomeMultidata().then((res) => {
+      // console.log(res);
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    });
+  },
+};
 </script>
 
 <style scoped>
-
+.home-nav {
+  background-color: var(--color-tint);
+}
 </style>
